816765bc89af53ec5aff771697fd014f12b7418a
Ivan Kulikov <gunmetal313@users.noreply.github.com>
2017-02-08 09:33:03 +0300

    bootstrap: Error: Configuration property "database.dbusername" is not defined was fixed

--- bootstrap.js
+++ bootstrap.js
@@ -8,11 +8,11 @@ if (config.has('database.connectionString')) {
     var db = new Sequelize(connectionString);
 } else {
 	var dbName = config.get('database.database');
-	var dbUsername = config.get('database.dbusername');
+	var dbUsername = config.get('database.username');
 	var dbPassword = config.get('database.password');
 	var dbHost = config.get('database.host');
-  var dbDialect = config.get('database.dialect');
-  var db = new Sequelize(dbName, dbUsername, dbPassword, {
+    var dbDialect = config.get('database.dialect');
+    var db = new Sequelize(dbName, dbUsername, dbPassword, {
         host:  dbHost,
         dialect: dbDialect
     });
